e94c9736ff81a9225677d4a94f4bbbe75c025381
suhail <suhailanzar00@gmail.com>
2024-09-02 16:00:40 +0530

    remove slash in url

--- src/app/services/user-service.service.ts
+++ src/app/services/user-service.service.ts
@@ -10,7 +10,7 @@ import { HttpHeaders } from '@angular/common/http';
 })
 export class UserServiceService {
 
-  BASE_URL:string = 'https://chat-poll-socketio-backend.onrender.com/'
+  BASE_URL:string = 'https://chat-poll-socketio-backend.onrender.com'
 
 
   httpOptions = {
